e82329e305f5f679ecfdd2c1c6f4a031fd4bde53
preddy97 <pranav@aporeto.com>
2016-06-16 12:22:16 -0700

    Updated create definitions

--- features/step_definitions/post.rb
+++ features/step_definitions/post.rb
@@ -2,7 +2,16 @@ require 'httparty'
 require 'test/unit/assertions'
 include Test::Unit::Assertions
 
-When /^(.*) (creates|updates) a (.*) in (.*):/ do |usr, _, str, table|
+When /^(.*) creates a (.*) in (.*):/ do |usr, _, str, table|
+  temp = table.hashes[0]
+  temp.each {|key, value| convToBoolean(temp, key,value) }
+  temp = temp.to_json
+  @last_response = HTTParty.post(convToURL(str),
+                                 body: temp,
+                                 headers: {"X-Namespace" => @users[usr]["namespace"]})
+end
+
+When /^(.*) updates a (.*) in (.*):/ do |usr, _, str, table|
   temp = table.hashes[0]
   temp.each {|key, value| convToBoolean(temp, key,value) }
   temp = temp.to_json
